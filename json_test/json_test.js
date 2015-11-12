aa2e7c652af78a86f104439abc5d88f30a8ca31b
nygeog <daniel.martin.sheehan@gmail.com>
2015-11-11 21:13:48 -0500

    update

---
+++ json_test/json_test.js
@@ -0,0 +1,9 @@
+<html>
+<head>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"> </script>
+
+<script>
+
+$.getJSON('mydata.json', function(data) {
+    //do stuff with your data here
+});
